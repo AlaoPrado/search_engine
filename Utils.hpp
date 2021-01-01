067ab8089f1051a2f62cb72b1d903845a3d820eb
Alan Prado <alanpradoaraujo@gmail.com>
2021-01-01 11:42:17 -0300

    fix: format Utils.hpp

--- Utils.hpp
+++ Utils.hpp
@@ -1,13 +1,14 @@
 #include <stdexcept>
 #include <string>
 
-namespace search_engine { 
+namespace search_engine {
 namespace utils {
 
-void inline assertTrue(const bool condition, const std::string message){
-  if(!condition){
+void inline assertTrue(const bool condition, const std::string message) {
+  if (!condition) {
     throw std::logic_error(message);
   }
 }
 
-}}
+} // namespace utils
+} // namespace search_engine
