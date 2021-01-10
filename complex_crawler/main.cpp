5ed9cce02372057bcc5cdf8d3d34e8c97145ace2
Alan Prado <alanpradoaraujo@gmail.com>
2021-01-09 22:41:41 -0300

    feat: add complex crawler folder with seed reader program and Makefile

---
+++ complex_crawler/main.cpp
@@ -0,0 +1,29 @@
+#include "../utils/Assert.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(const int argc, const char **argv) {
+  try {
+    search_engine::utils::assertTrue(
+        argc > 1, "Error: a file name must be passed by parameter");
+
+    const std::string fileName = argv[1];
+    std::ifstream seedFile(fileName);
+
+    search_engine::utils::assertTrue(seedFile.is_open(),
+                                     "Error: invalid file name");
+
+    std::vector<std::string> seedList;
+    std::string line;
+    while (std::getline(seedFile, line)) {
+      seedList.push_back(line);
+    }
+
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
+}
