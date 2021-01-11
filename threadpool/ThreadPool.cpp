abb074b085b1f823b678cbeec73e9938bfb01c40
Alan Prado <alanpradoaraujo@gmail.com>
2021-01-11 20:50:55 -0300

    fix(threadpool): add ThreadParam methods implementatio

--- threadpool/ThreadPool.cpp
+++ threadpool/ThreadPool.cpp
@@ -1,28 +1,50 @@
 #include "ThreadPool.hpp"
 #include "../utils/SynchronizedQueue.hpp"
+#include <pthread.h>
 #include <stdexcept>
+#include <utility>
 
 namespace search_engine {
 
+ThreadParam::ThreadParam(utils::SynchronizedQueue<Task> *workQueue,
+                         pthread_mutex_t *memoryMutex)
+    : workQueue(workQueue), memoryMutex(memoryMutex) {}
+
+utils::SynchronizedQueue<Task> *ThreadParam::getWorkQueue() {
+  return this->workQueue;
+}
+
+pthread_mutex_t *ThreadParam::getMemoryMutex() { return this->memoryMutex; }
+
 void *ThreadPool::threadRun(void *param) {
   Task *task;
-  utils::SynchronizedQueue<Task> *workQueue;
-  workQueue = reinterpret_cast<utils::SynchronizedQueue<Task> *>(param);
+  ThreadParam *threadParam = reinterpret_cast<ThreadParam *>(param);
+  utils::SynchronizedQueue<Task> *workQueue = threadParam->getWorkQueue();
+  pthread_mutex_t *memoryMutex = threadParam->getMemoryMutex();
   task = workQueue->pop();
   while (task != NULL) {
     task->run();
+
+    if (memoryMutex != NULL) {
+      pthread_mutex_lock(memoryMutex);
+      delete task;
+      pthread_mutex_unlock(memoryMutex);
+    }
+
     task = workQueue->pop();
   }
   return NULL;
 }
 
-ThreadPool::ThreadPool(int size) {
+ThreadPool::ThreadPool(int size, pthread_mutex_t *memoryMutex) {
   this->size = size;
+  this->memoryMutex = memoryMutex;
   this->pool = new pthread_t[size];
-  this->workQueue = new utils::SynchronizedQueue<Task>();
+  this->workQueue = new utils::SynchronizedQueue<Task>(this->memoryMutex);
+  this->threadParam = new ThreadParam(this->workQueue, this->memoryMutex);
   for (int i = 0; i < this->size; i++) {
     if (!pthread_create(&(this->pool[i]), NULL, this->threadRun,
-                        (void *)this->workQueue)) {
+                        (void *)this->threadParam)) {
       throw std::logic_error("Error: failed to create thread " + i);
     }
   }
@@ -35,6 +57,7 @@ ThreadPool::~ThreadPool() {
     pthread_join(this->pool[i], NULL);
   delete this->workQueue;
   delete this->pool;
+  delete this->threadParam;
 }
 
 void ThreadPool::addTask(Task *task) { workQueue->push(task); }
--- threadpool/ThreadPool.hpp
+++ threadpool/ThreadPool.hpp
@@ -5,16 +5,30 @@
 #include <pthread.h>
 
 namespace search_engine {
-  
+
+class ThreadParam {
+private:
+  utils::SynchronizedQueue<Task> *workQueue;
+  pthread_mutex_t *memoryMutex;
+
+public:
+  ThreadParam(utils::SynchronizedQueue<Task> *workQueue,
+              pthread_mutex_t *memoryMutex);
+  utils::SynchronizedQueue<Task> *getWorkQueue();
+  pthread_mutex_t *getMemoryMutex();
+};
+
 class ThreadPool {
 private:
   pthread_t *pool;
   utils::SynchronizedQueue<Task> *workQueue;
+  pthread_mutex_t *memoryMutex;
+  ThreadParam *threadParam;
 
   static void *threadRun(void *);
 
 public:
-  ThreadPool(int size);
+  ThreadPool(int size, pthread_mutex_t *memoryMutex = NULL);
   ~ThreadPool();
   void addTask(Task *task);
   int getSize();
