ab097e4810fb5e11f5709fc339d886c2ee7a168c
Alan Prado <alanpradoaraujo@gmail.com>
2021-01-27 21:48:49 -0300

    fix(html-parser): complete getTagSeparator

--- html_parser/HtmlParser.cpp
+++ html_parser/HtmlParser.cpp
@@ -12,7 +12,20 @@ bool HtmlParser::isTagContentAvoidable(GumboTag tag) {
 std::string HtmlParser::getTagSeparator(GumboTag tag) {
   if (tag == GUMBO_TAG_P || tag == GUMBO_TAG_TITLE || tag == GUMBO_TAG_H1 ||
       tag == GUMBO_TAG_H2 || tag == GUMBO_TAG_H3 || tag == GUMBO_TAG_H4 ||
-      tag == GUMBO_TAG_H5 || tag == GUMBO_TAG_H6) {
+      tag == GUMBO_TAG_H5 || tag == GUMBO_TAG_H6 || tag == GUMBO_TAG_ARTICLE ||
+      tag == GUMBO_TAG_SECTION || tag == GUMBO_TAG_NAV ||
+      tag == GUMBO_TAG_ASIDE || tag == GUMBO_TAG_HEAD ||
+      tag == GUMBO_TAG_HGROUP || tag == GUMBO_TAG_HEADER ||
+      tag == GUMBO_TAG_FOOTER || tag == GUMBO_TAG_ADDRESS ||
+      tag == GUMBO_TAG_BLOCKQUOTE || tag == GUMBO_TAG_FIGCAPTION ||
+      tag == GUMBO_TAG_MAIN || tag == GUMBO_TAG_TABLE ||
+      tag == GUMBO_TAG_CAPTION || tag == GUMBO_TAG_TR || tag == GUMBO_TAG_TD ||
+      tag == GUMBO_TAG_TH || tag == GUMBO_TAG_LEGEND ||
+      tag == GUMBO_TAG_LABEL || tag == GUMBO_TAG_BUTTON ||
+      tag == GUMBO_TAG_OPTION || tag == GUMBO_TAG_TEXTAREA ||
+      tag == GUMBO_TAG_PROGRESS || tag == GUMBO_TAG_METER ||
+      tag == GUMBO_TAG_DETAILS || tag == GUMBO_TAG_SUMMARY ||
+      tag == GUMBO_TAG_MENUITEM) {
     return "\n";
   }
 
@@ -25,7 +38,7 @@ std::string HtmlParser::cleanText(GumboNode *node) {
   } else if (node->type == GUMBO_NODE_ELEMENT &&
              !isTagContentAvoidable(node->v.element.tag)) {
 
-    std::string childrenText = "";
+    std::string childrenText("");
     GumboVector *children = &node->v.element.children;
 
     for (std::size_t i = 0; i < children->length; ++i) {
@@ -35,6 +48,7 @@ std::string HtmlParser::cleanText(GumboNode *node) {
         childrenText += childText;
       }
     }
+
     if (!childrenText.empty()) {
       childrenText += getTagSeparator(node->v.element.tag);
     }
